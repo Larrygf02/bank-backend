41b92e7fce1edde01c01e237941e065b3663a42e
larrygf02 <raulgf_02@hotmail.com>
2019-12-29 12:10:32 -0500

    Crud persons

--- src/controllers/person.controller.js
+++ src/controllers/person.controller.js
@@ -26,3 +26,76 @@ export async function createPerson(req, res) {
         })
     }
 }
+
+export async function getPersons(req, res) {
+    const persons = await Persons.findAll()
+    res.json({
+        data: persons
+    })
+}
+
+export async function getOnePerson(req, res) {
+    try {
+        const { id } = req.params;
+        const person = await Persons.findOne({
+            where: {
+                id
+            }
+        })
+        res.json({
+            data: person
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Something goes wrong',
+            data: {}
+        })
+    }
+}
+
+export async function deletePerson(req, res) {
+    try {        
+        const { id } = req.params
+        const deleteRowCount = Persons.destroy({
+            where: {
+                id
+            }
+        })
+        res.json({
+            message: 'Project Deleted Succesfully',
+            count: deleteRowCount
+        })
+    } catch (error) {
+        res.json({
+            message: 'Something was wrong'
+        })
+    }
+}
+
+export async function updatePerson(req, res) {
+    const { id } = req.params
+    const { name, surname, dni, birth_date } = req.body
+    const person = await Persons.findOne({
+        where: {
+            id
+        }
+    })
+    if (person) {
+        person.update({
+            name,
+            surname,
+            dni,
+            birth_date
+        })
+        return res.json({
+            data: person,
+            message: 'Updated succesfully'
+        })
+    }else{
+        return res.json({
+            data: null,
+            message: `Person not found with id ${id}`
+        })
+    }
+}
--- src/routes/persons.js
+++ src/routes/persons.js
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 const router = Router()
 
-import { createPerson } from '../controllers/person.controller'
+import { createPerson, getPersons ,getOnePerson, deletePerson, updatePerson } from '../controllers/person.controller'
 
 // api/persons/
 router.post('/', createPerson)
+router.get('/', getPersons)
+router.get('/:id', getOnePerson)
+router.delete('/:id', deletePerson)
+router.patch('/:id', updatePerson)
+
 export default router;
